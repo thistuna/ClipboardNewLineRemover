ef485e143e3b57a2f8ff5781683db4992f4b2d28
thistuna <thistuna@gmail.com>
2022-01-05 23:08:51 +0900

    [add] クリップボードの操作を追加

--- ClipboardNewLineRemover/Form1.cs
+++ ClipboardNewLineRemover/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+// 参考：https://lets-csharp.com/how-to-clipboard-listener/
 
 namespace ClipboardNewLineRemover
 {
@@ -17,5 +20,61 @@ namespace ClipboardNewLineRemover
             InitializeComponent();
         }
 
+        [DllImport("user32.dll", SetLastError = true)]
+        private extern static void AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private extern static void RemoveClipboardFormatListener(IntPtr hwnd);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AddClipboardFormatListener(Handle);
+        }
+
+        
+        void OnClipboardUpdate()
+        {
+            // クリップボードのデータが変更された
+
+            // クリップボードがテキストだったら
+            if (Clipboard.ContainsText())
+            {
+                string input = Clipboard.GetText();
+                input = input.Replace("\r\n", " ");
+                input = input.Replace("\n", " ");
+                input = input.Replace("\r", " ");
+                bool end = false;
+                while (!end)
+                {
+                    string replaced = input.Replace("  ", " ");
+                    if (replaced == input)
+                    {
+                        end = true;
+                    }
+                    input = replaced;
+                }
+                RemoveClipboardFormatListener(Handle);
+                Clipboard.SetText(input);
+                AddClipboardFormatListener(Handle);
+            }
+        }
+
+        private const int WM_CLIPBOARDUPDATE = 0x31D;
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                OnClipboardUpdate();
+                m.Result = IntPtr.Zero;
+            }
+            else
+                base.WndProc(ref m);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            RemoveClipboardFormatListener(Handle);
+        }
     }
 }
